c0735639c31a4746a17a1cb6beebe3c8ff41a205
iyuuya <i.yuuya@gmail.com>
2019-11-27 17:19:23 +0900

    add excludes configuration
    resolve #2

--- spec/sidekiq/metrics/configuration_spec.rb
+++ spec/sidekiq/metrics/configuration_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe Sidekiq::Metrics::Configuration do
       expect { configuration.adapter = :foo }.to change { configuration.adapter }.to(:foo)
     end
   end
+
+  describe '#excludes' do
+    subject { described_class.new.excludes }
+
+    it 'default value is a Set instance' do
+      is_expected.to be_a Set
+      is_expected.to be_empty
+    end
+
+    it 'can add value' do
+      expect { subject << 'HelloWorker' }.to change { subject.count }.by(1)
+    end
+  end
+
+  describe 'not #excludes=(v)' do
+    it 'can not set value' do
+      expect { configuration.excludes = [] }.to raise_error
+    end
+  end
 end
