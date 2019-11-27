b697ab6f09d4b092fa5eda2358c10526c8e5409c
iyuuya <i.yuuya@gmail.com>
2019-11-27 15:15:08 +0900

    add conifgure specs

--- spec/sidekiq/metrics_spec.rb
+++ spec/sidekiq/metrics_spec.rb
@@ -3,6 +3,16 @@
 require 'spec_helper'
 
 RSpec.describe Sidekiq::Metrics do
-  xdescribe '#configuration'
-  xdescribe '#configure'
+  describe '#configuration' do
+    subject { described_class.configuration }
+    it { is_expected.to be_a Sidekiq::Metrics::Configuration }
+  end
+
+  describe '#configure' do
+    it 'configure metrics in block' do
+      expect {
+        Sidekiq::Metrics.configure { |config| config.adapter = :hello }
+      }.to change{ Sidekiq::Metrics.configuration.adapter }.to(:hello)
+    end
+  end
 end
