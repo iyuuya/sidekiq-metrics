e82816cecef38015cbc5e5657e65b7214a8403b8
iyuuya <i.yuuya@gmail.com>
2019-11-27 13:04:33 +0900

    fix spec

--- spec/sidekiq/metrics/adapter/base_spec.rb
+++ spec/sidekiq/metrics/adapter/base_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe Sidekiq::Metrics::Adapter::Base do
   describe '#write(worker_status)' do
-    it { expect { described_class.new.write }.to raise_error NotImplementedError, 'Subclasses must define `write`.' }
+    it { expect { described_class.new.write({}) }.to raise_error NotImplementedError, 'Subclasses must define `write`.' }
   end
 end
